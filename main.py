51fbbf2b39e51e785e5b24a957810d3c001634b4
uak <4626956-uak@users.noreply.gitlab.com>
2022-04-16 14:05:06 +0300

    simplified example and updated font

--- main.py
+++ main.py
@@ -7,9 +7,24 @@ App demonstrating a Text input field which accepts Arabic script in kivy
 import arabic_reshaper
 from bidi.algorithm import get_display
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 
+# You can have KV language in a file named test.kv 
+# if your app Name is TestAppand comment line
+# `Builder.load_string(KV)` 
+
+KV = """
+<Ar_text@TextInput>:
+    text: "whatever"
+    multiline: 0
+    size_hint: 1,1
+    font_name: "data/unifont-14.0.02.ttf" # the font you want to use
+    font_size: 26
+    padding_y: [15,0] # can be changed
+    padding_x: [self.size[0]-self._get_text_width(max(self._lines, key=len), self.tab_width, self._label_cached)-10,8]
+"""
 
 class Ar_text(TextInput):
     max_chars = NumericProperty(20)  # maximum character allowed
@@ -36,6 +51,7 @@ class Ar_text(TextInput):
 class TestApp(App):
 
     def build(self):
+        Builder.load_string(KV)
         return Ar_text()
 
 
